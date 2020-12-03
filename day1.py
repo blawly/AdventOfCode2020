e4fe7c7575f214d34b4e16bf5003bd9ef8fe343a
blawly <bauer.marton97@gmail.com>
2020-12-03 17:35:59 +0100

    Cleanup on day2

--- day1.py
+++ day1.py
@@ -3,9 +3,11 @@ numbers = [0 for x in range(200)]
 for x in range(200):
     numbers[x] = int(f.readline())
     for y in range(x):
+        #Part One
         if numbers[x] + numbers[y] == 2020:
-            print(numbers[x] * numbers[y]) #Part One
+            print(numbers[x] * numbers[y])
+        #Part Two
         elif numbers[x] + numbers[y] < 2020:
             for z in range(y):
                 if numbers[x] + numbers[y] + numbers[z] == 2020:
-                    print(numbers[x] * numbers[y] * numbers[z]) #Part Two
+                    print(numbers[x] * numbers[y] * numbers[z])
--- day2.py
+++ day2.py
@@ -1,16 +1,16 @@
 f = open('day2.txt')
-lines = [0 for x in range(1000)]
 validOne = 0
 validTwo = 0
-for i in range(1000):
-    lines[i] = f.readline()
-    one = int(lines[i].split('-')[0])
-    two = int(lines[i].split('-')[1].split(' ')[0])
-    letter = lines[i].split(' ')[1].split(':')[0]
-    word = lines[i].split(' ')[2]
+for line in f:
+    one = int(line.split('-')[0])
+    two = int(line.split('-')[1].split(' ')[0])
+    letter = line.split(' ')[1].split(':')[0]
+    word = line.split(' ')[2]
     count = word.count(letter)
+    #Part One
     if count in range(one, two + 1):
         validOne += 1
+    #Part Two
     if (word[one - 1] == letter and word[two - 1] != letter) != (word[one - 1] != letter and word[two - 1] == letter):
         validTwo += 1
 print(validOne)
