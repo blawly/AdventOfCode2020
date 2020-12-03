756d25d85a3ae453e7d95e039a9dbcb3d72fe717
blawly <bauer.marton97@gmail.com>
2020-12-03 10:57:02 +0100

    Day3

---
+++ day3.py
@@ -0,0 +1,27 @@
+f = open("day3.txt")
+lines = []
+for line in f:
+    lines.append(line)
+y = 0
+trees = 0
+for line in lines:
+    if line != '':
+        if line[y] == '#':
+            trees += 1
+        y += 3
+        y = y % (len(line) - 1)
+print(trees)
+
+product = 1
+slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
+for slope in slopes:
+    y = 0
+    trees = 0
+    for line in lines[::slope[0]]:
+        if line != '':
+            if line[y] == '#':
+                trees += 1
+            y += slope[1]
+            y = y % (len(line) - 1)
+    product *= trees
+print(product)
