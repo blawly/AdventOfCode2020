490d5e96bd911fbfe7708484e3b9c9ae231b8d32
blawly <bauer.marton97@gmail.com>
2020-12-03 11:07:31 +0100

    Added comments to day3.py

--- day3.py
+++ day3.py
@@ -2,6 +2,7 @@ f = open("day3.txt")
 lines = []
 for line in f:
     lines.append(line)
+#Part One
 y = 0
 trees = 0
 for line in lines:
@@ -11,7 +12,7 @@ for line in lines:
         y += 3
         y = y % (len(line) - 1)
 print(trees)
-
+#Part Two
 product = 1
 slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
 for slope in slopes:
