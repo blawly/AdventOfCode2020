dfd3bfc7335bfa254910ae83f772fad449198b04
blawly <bauer.marton97@gmail.com>
2020-12-05 14:11:46 +0100

    Easier method to generate ids in day5

--- day5.py
+++ day5.py
@@ -1,19 +1,12 @@
 f = open("day5.txt")
 ids = []
 for line in f:
-    rowCode = line[0:7]
-    colCode = line[7:10]
-    row = 0
-    for char in rowCode:
-        row *= 2
-        if char == 'B':
-            row += 1
-    col = 0
-    for char in colCode:
-        col *= 2
-        if char == 'R':
-            col += 1
-    ids.append(row * 8 + col)
+    pid = 0
+    for char in line.rstrip('\n'):
+        pid *= 2
+        if char in ['B', 'R']:
+            pid += 1
+    ids.append(pid)
 #Part One
 maxid = 0
 for pid in ids:
@@ -22,7 +15,7 @@ for pid in ids:
 print(maxid)
 #Part Two
 for myid in range(1024):
-    if (myid not in ids and 
-        myid - 1 in ids and 
+    if (myid not in ids and
+        myid - 1 in ids and
         myid + 1 in ids):
             print(myid)
