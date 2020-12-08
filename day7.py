e13ca9787dff306320f2c4b19058bc609610816b
blawly <bauer.marton97@gmail.com>
2020-12-08 17:50:54 +0100

    Added some comments to day7

--- day7.py
+++ day7.py
@@ -1,11 +1,14 @@
 f = open('input/day7.txt')
+#Dictionary for the rules, keys are bag colors
 rules = dict()
 for line in f:
     words = line.split()
+    #Dictionaries for each rule, values for the main dictionary
     ruleDict = dict()
     if words[4] != 'no':
         for i in range(4, len(words), 4):
             ruleDict[words[i + 1] + words[i + 2]] = int(words[i])
+    #Filling the main dictionary
     rules[words[0] + words[1]] = ruleDict
 #Part One
 def canContain(color):
