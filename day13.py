636910c769dfd0bf552e554ee134e76208712c99
blawly <bauer.marton97@gmail.com>
2020-12-13 17:03:22 +0100

    Tuples instead of lists for individual buses and minWait

--- day13.py
+++ day13.py
@@ -6,17 +6,17 @@ with open('input/day13.txt') as f:
     i = 0
     for bus in f.readline().rstrip('\n').split(','):
         if bus != 'x':
-            buses.append([i, int(bus)])
+            buses.append((i, int(bus)))
         i += 1
 
 buses.sort(reverse = True, key = lambda x: x[1])
 
 # Part One
-minWait = [-1, buses[0][1]]
+minWait = (-1, buses[0][1])
 for bus in buses:
     wait = math.ceil(timestamp / bus[1]) * bus[1] - timestamp
     if wait < minWait[1]:
-        minWait = [bus[1], wait]
+        minWait = (bus[1], wait)
 print(math.prod(minWait))
 
 # Part Two
