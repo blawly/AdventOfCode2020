da7201b8e81b083efa1d712e1f21769ecfba681d
blawly <bauer.marton97@gmail.com>
2020-12-18 16:58:08 +0100

    Day18 Part Two

--- day18.py
+++ day18.py
@@ -1,4 +1,4 @@
-def subExpression(expression, start):
+def sliceByParetheses(expression, start):
     depth = 0
     for i in range(start, len(expression)):
         if expression[i] == '(':
@@ -8,15 +8,41 @@ def subExpression(expression, start):
         if depth == 0:
             return expression[start + 1 : i]
 
+def parenthesizePluses(expression):
+    i = 0
+    while i < len(expression):
+        if expression[i] == '+':
+            depth = 0
+            for j in range(i - 1, - 1, - 1):
+                if expression[j] == ')':
+                    depth += 1
+                elif expression[j] == '(':
+                    depth -= 1
+                if expression[j] not in '+*' and depth == 0:
+                    start = j
+                    break
+            for j in range(i + 1, len(expression)):
+                if expression[j] == '(':
+                    depth += 1
+                elif expression[j] == ')':
+                    depth -= 1
+                if expression[j] not in '+*' and depth == 0:
+                    end = j + 1
+                    break
+            expression = expression[: start] + '(' + expression[start : end] + ')' + expression[end :]
+            i += 1
+        i += 1
+    return(expression)
+    
 def solve(expression):
     result = 0
     operator = ''
     i = 0
     while i < len(expression):
         if expression[i] == '(':
-            subExp = subExpression(expression, i)
-            i += len(subExp) + 1
-            operand = solve(subExp)
+            subExpression = sliceByParetheses(expression, i)
+            i += len(subExpression) + 1
+            operand = solve(subExpression)
             if operator == '*':
                 result *= operand
             else:
@@ -41,3 +67,8 @@ partOne = 0
 for expression in expressions:
     partOne += solve(expression)
 print(partOne)
+
+partTwo = 0
+for expression in expressions:
+    partTwo += solve(parenthesizePluses(expression))
+print(partTwo)
